d918058f4c1ee7c17001014948c71bacd38666ef
John Niyontwali <nijohn0006@gmail.com>
2022-03-19 10:53:53 +0200

    The following changes were made: - a list was provided - navbar component created
    Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to React App'
+  const num = 1
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Container div
+    <div className='mx-[100px]'>
+      <Navbar />
+      {/* Heading */}
+      <h1 className='text-center mt-10 text-blue-600 font-bold text-4xl'> {title} </h1>
+      <p className='font-bold text-center mt-3'>This is the {num}st  paragraph</p>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <div className="bg-gray-200">
+            <nav>
+                <ul className="flex justify-between">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/Services">About</a></li>
+                    <li><a href="/login">login</a></li>
+                </ul>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
